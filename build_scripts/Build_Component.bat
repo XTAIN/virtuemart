89f1851515496753109f0928f2d09ac2bdc9e33f
soeren_nb <soeren_nb@87cc427b-4a1e-0410-b729-b2260ec0f063>
2006-12-13 20:24:17 +0000

    updated to use ~ appendix
    git-svn-id: http://dev.virtuemart.net/svn/virtuemart/trunk@572 87cc427b-4a1e-0410-b729-b2260ec0f063

--- build_scripts/Build_Component.bat
+++ build_scripts/Build_Component.bat
@@ -38,13 +38,13 @@ rem ----------------------------------------------------------------------------
 %zip% a -ttar -r %SVN_PATH%\admin_files.tar %SVN_PATH%\classes %SVN_PATH%\html %SVN_PATH%\languages %SVN_PATH%\sql
 %zip% d %SVN_PATH%\admin_files.tar languages\*.php -x!languages\english.php
 %zip% d -r %SVN_PATH%\admin_files.tar .svn\
-%zip% a -tgzip %SVN_PATH%\admin_files.tar.gz %SVN_PATH%\admin_files.tar 
+%zip% a -tgzip %SVN_PATH%\admin_files.tar.gz~ %SVN_PATH%\admin_files.tar 
 del %SVN_PATH%\admin_files.tar
 
 %zip% a -ttar -r %SVN_PATH%\frontend_files.tar %SVN_PATH%\js %SVN_PATH%\shop_image %SVN_PATH%\themes %SVN_PATH%\fetchscript.php %SVN_PATH%\virtuemart.php %SVN_PATH%\virtuemart_parser.php %SVN_PATH%\show_image_in_imgtag.php
 %zip% a -ttar %SVN_PATH%\frontend_files.tar %SVN_PATH%\index.html
 %zip% d -r %SVN_PATH%\frontend_files.tar .svn\
-%zip% a -tgzip %SVN_PATH%\frontend_files.tar.gz %SVN_PATH%\frontend_files.tar 
+%zip% a -tgzip %SVN_PATH%\frontend_files.tar.gz~ %SVN_PATH%\frontend_files.tar 
 del %SVN_PATH%\frontend_files.tar
 
 %zip% a -ttar -r- %SVN_PATH%\com_virtuemart.tar %SVN_PATH%\*.*
@@ -53,6 +53,6 @@ del %SVN_PATH%\frontend_files.tar
 FOR /F "tokens=1,2,3,4 delims=/. " %%a in ('date/T') do set CDATE=%%c%%b%%a
 %zip% a -tgzip %SVN_PATH%\com_virtuemart_%CDATE%.tar.gz %SVN_PATH%\com_virtuemart.tar
 
-del %SVN_PATH%\admin_files.tar.gz
-del %SVN_PATH%\frontend_files.tar.gz
+del %SVN_PATH%\admin_files.tar.gz~
+del %SVN_PATH%\frontend_files.tar.gz~
 del %SVN_PATH%\com_virtuemart.tar
