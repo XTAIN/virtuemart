a60617881d573e0ace68c0b394dfa73d5675aea4
Milbo <Milbo@87cc427b-4a1e-0410-b729-b2260ec0f063>
2011-10-18 21:00:06 +0000

    synchro for release
    git-svn-id: http://dev.virtuemart.net/svn/virtuemart/trunk@4458 87cc427b-4a1e-0410-b729-b2260ec0f063

--- ModulesPluginsInstaller/admin/admin.virtuemart_allinone.php
+++ ModulesPluginsInstaller/admin/admin.virtuemart_allinone.php
@@ -3,20 +3,20 @@
  * @package LiveUpdate
  * @copyright Copyright ©2011 Nicholas K. Dionysopoulos / AkeebaBackup.com
  * @license GNU LGPLv3 or later <http://www.gnu.org/copyleft/lesser.html>
- * 
+ *
  * One-click updater for Joomla! extensions
  * Copyright (C) 2011  Nicholas K. Dionysopoulos / AkeebaBackup.com
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -25,6 +25,8 @@ defined('_JEXEC') or die();
 
 require_once dirname(__FILE__).'/classes/abstractconfig.php';
 require_once dirname(__FILE__).'/config.php';
+
+LiveUpdate::handleRequest();
 
 class LiveUpdate
 {
@@ -40,7 +42,7 @@ class LiveUpdate
 		$jlang->load('liveupdate', $basePath, $jlang->getDefault(), true); // Load the site's default language
 		$jlang->load('liveupdate', $basePath, null, true); // Load the currently selected language
 	}
-	
+
 	/**
 	 * Handles requests to the "liveupdate" view which is used to display
 	 * update information and perform the live updates
@@ -49,14 +51,14 @@ class LiveUpdate
 	{
 		// Load language strings
 		self::loadLanguage();
-		
+
 		// Load the controller and let it run the show
 		require_once dirname(__FILE__).'/classes/controller.php';
 		$controller = new LiveUpdateController();
 		$controller->execute(JRequest::getCmd('task','overview'));
 		$controller->redirect();
 	}
-	
+
 	/**
 	 * Returns update information about your extension, based on your configuration settings
 	 * @return stdClass
@@ -68,30 +70,30 @@ class LiveUpdate
 		$info = $update->getUpdateInformation($force);
 		$hasUpdates = $update->hasUpdates();
 		$info->hasUpdates = $hasUpdates;
-		
+
 		$config = LiveUpdateConfig::getInstance();
 		$extInfo = $config->getExtensionInformation();
-		
+
 		$info->extInfo = (object)$extInfo;
-		
+
 		return $info;
 	}
-	
+
 	public static function getIcon($config=array())
 	{
 		// Load language strings
 		self::loadLanguage();
-		
+
 		$defaultConfig = array(
 			'option'			=> JRequest::getCmd('option',''),
 			'view'				=> 'liveupdate',
 			'mediaurl'			=> JURI::base().'components/'.JRequest::getCmd('option','').'/liveupdate/assets/'
 		);
 		$c = array_merge($defaultConfig, $config);
-		
+
 		$url = 'index.php?option='.$c['option'].'&view='.$c['view'];
 		$img = $c['mediaurl'];
-		
+
 		$updateInfo = self::getUpdateInformation();
 		if(!$updateInfo->supported) {
 			// Unsupported
@@ -114,7 +116,7 @@ class LiveUpdate
 			$img .= 'current-32.png';
 			$lbl = JText::_('LIVEUPDATE_ICON_CURRENT');
 		}
-		
+
 		return '<div class="icon"><a href="'.$url.'">'.
 			'<div><img src="'.$img.'" width="32" height="32" border="0" align="middle" style="float: none" /></div>'.
 			'<span class="'.$class.'">'.$lbl.'</span></a></div>';
