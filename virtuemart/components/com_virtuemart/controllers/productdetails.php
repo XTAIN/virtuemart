39ae705d75a9c437bdec47dc8930f747da57c0f7
Milbo <Milbo@87cc427b-4a1e-0410-b729-b2260ec0f063>
2014-06-12 15:08:59 +0000

    removed old functions ins shopfunctions.php added getTablesBySql, so we can read the insert lines of sql files fixed order up and order down for ordering, if the selected ordering of the list is NOT "ordering" Added menu items for support moved fixing of BE module to AIO basePrice is suitable replaced by basePriceVariant moved if replaced old searchtrigger by 			$dispatcher->trigger('plgVmBeforeProductSearch', array(&$select, &$joinedTables, &$where, &$groupBy, &$orderBy,&$joinLang)); vendorId of a product created by a subvendor is directly correctly added Added new attribute to shoppergroups, defining if they remove the default group from a shopper or not more work for multivendor and more minors
    git-svn-id: http://dev.virtuemart.net/svn/virtuemart/trunk@8024 87cc427b-4a1e-0410-b729-b2260ec0f063

--- virtuemart/components/com_virtuemart/controllers/productdetails.php
+++ virtuemart/components/com_virtuemart/controllers/productdetails.php
@@ -13,7 +13,7 @@
  * to the GNU General Public License, and as distributed it includes or
  * is derivative of works licensed under the GNU General Public License or
  * other free or open source software licenses.
- * @version $Id: productdetails.php 7821 2014-04-08 11:07:57Z Milbo $
+ * @version $Id$
  */
 
 // Check to ensure this file is included in Joomla!
@@ -310,16 +310,12 @@ class VirtueMartControllerProductdetails extends JControllerLegacy {
 
 		$product_model = VmModel::getModel ('product');
 
-		//VmConfig::$echoDebug=1;
 
-		//if(isset($customProductData[$virtuemart_product_id])){
+		if(!empty($virtuemart_product_id)){
 			$prices = $product_model->getPrice ($virtuemart_product_id, $quantity);
-			//vmdebug('Wie siehts aus? ',$customProductData,$prices);
-		//} else {
-			//VmConfig::$echoDebug=true;
-			//vmdebug('recalculate',$customProductData);
-			//jexit ();
-		//}
+		} else {
+			jexit ();
+		}
 		$priceFormated = array();
 		if (!class_exists ('CurrencyDisplay')) {
 			require(JPATH_VM_ADMINISTRATOR . DS . 'helpers' . DS . 'currencydisplay.php');
